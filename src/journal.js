483003be4c8693844fe3c818c415eb46d8b24771
Victor Thompson <kelechithompson26@gmail.com>
2023-04-05 12:09:35 +0100

    add charCounter-prototype

--- src/journal.js
+++ src/journal.js
@@ -11,3 +11,19 @@ Journal.prototype.wordCounter = function () {
   totalWords = wordArray.length;
   return totalWords;
 };
+
+Journal.prototype.charCounter = function () {
+  let vowelArr = ["a", "i", "o", "e", "u"];
+  let totalVowels = 0;
+  let totalConsonants = 0;
+
+  for (let i = 0; i < this.body.length; i++) {
+    if (vowelArr.includes(this.body[i])) {
+      totalVowels++;
+    }
+    else if (this.body[i] != " ") {
+      totalConsonants++;
+    }
+  }
+  return [totalVowels, totalConsonants];
+};
