d09dde5dc5e9a1ed002c256c947f7bd3d5f672f9
Victor Thompson <kelechithompson26@gmail.com>
2023-04-05 12:08:16 +0100

    add wordCounter-prototype

--- src/journal.js
+++ src/journal.js
@@ -2,3 +2,12 @@ export default function Journal(title, body) {
   this.title = title;
   this.body = body;
 }
+Journal.prototype.wordCounter = function () {
+  if(this.body.trim().length === 0) {
+    return 0;
+  }
+  let totalWords = 0;
+  let wordArray = this.body.split(" ");
+  totalWords = wordArray.length;
+  return totalWords;
+};
